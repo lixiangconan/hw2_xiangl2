ac7bdfff6bf3df689033951ada64ffbe3e6d843f
lixiangconan <lixiangconan@gmail.com>
2013-09-21 18:35:07 -0400

    Finished. Scoring using n-gram matching.

---
+++ hw2-xiangl2/src/main/java/edu/cmu/xiangl2/annotators/Evaluation.java
@@ -0,0 +1,111 @@
+package edu.cmu.xiangl2.annotators;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIndex;
+import org.apache.uima.jcas.JCas;
+import edu.cmu.deiis.types.Answer;
+import edu.cmu.deiis.types.AnswerScore;
+
+/**
+ * This class outputs answers to the question and measures the performance of the system.
+ * <p>
+ * The Evaluator choose N answers with the highest scores as the answers given by the system, then
+ * calculate the precision according to how many answers given by the system is correct.
+ */
+public class Evaluation extends JCasAnnotator_ImplBase {
+
+  /**
+   * The major evaluation process.
+   * <p>
+   * This method firstly order the answers according to their score. Then, N sentences with the
+   * highest scores are chosen as the output answers. Finally, it counts how many chosen answers are
+   * actually correct, and calculate a precision to measure the system performance.
+   */
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    int N = getCorrectNumber(aJCas);
+    System.out.println("*******************************");
+    System.out.println("N=" + N);
+    System.out.println("*******************************");
+    List<AnswerScore> correctAnswer = null;
+    correctAnswer = chooseCorrectAnswer(aJCas, N);
+    System.out.println("Answers given by the system:");
+    for (int i = 0; i < correctAnswer.size(); i++) {
+      System.out.println(aJCas.getDocumentText().substring(correctAnswer.get(i).getBegin(),
+              correctAnswer.get(i).getEnd())
+              + "Score: " + correctAnswer.get(i).getScore());
+    }
+    System.out.println("*******************************");
+    int correctAnswerNumber = 0;
+    double precision;
+    for (int i = 0; i < correctAnswer.size(); i++) {
+      if (correctAnswer.get(i).getAnswer().getIsCorrect())
+        correctAnswerNumber = correctAnswerNumber + 1;
+    }
+    precision = correctAnswerNumber / (double) N;
+    System.out.println("Precision: " + precision);
+    System.out.println("*******************************");
+  }
+
+  /**
+   * This method could count the number of correct answers according to the input information.
+   * 
+   * @param aJCas
+   * @return
+   */
+  private int getCorrectNumber(JCas aJCas) {
+    FSIndex<?> answerIndex = aJCas.getAnnotationIndex(Answer.type);
+    Iterator<?> answerIter = answerIndex.iterator();
+    int N = 0;
+    while (answerIter.hasNext()) {
+      Answer answer = (Answer) answerIter.next();
+      if (answer.getIsCorrect())
+        N = N + 1;
+    }
+    return N;
+  }
+
+  private class ComparatorAnswerScore implements Comparator {
+
+    public int compare(Object arg0, Object arg1) {
+      AnswerScore answerScore0 = (AnswerScore) arg0;
+      AnswerScore answerScore1 = (AnswerScore) arg1;
+      double delta = answerScore0.getScore() - answerScore1.getScore();
+      if (delta > 0.00001)
+        return 1;
+      if (delta < -0.00001)
+        return -1;
+      return 0;
+    }
+  }
+
+  /*
+   * This method find n answers with the highest score.
+   */
+  private List<AnswerScore> chooseCorrectAnswer(JCas aJCas, int correctNuber) {
+    List<AnswerScore> result = new ArrayList<AnswerScore>();
+    List<AnswerScore> answerScoreList = new ArrayList<AnswerScore>();
+
+    FSIndex<?> answerScoreIndex = aJCas.getAnnotationIndex(AnswerScore.type);
+    Iterator<?> answerScoreIter = answerScoreIndex.iterator();
+
+    while (answerScoreIter.hasNext()) {
+      AnswerScore answerScore = (AnswerScore) answerScoreIter.next();
+      answerScoreList.add(answerScore);
+    }
+
+    ComparatorAnswerScore comparator = new ComparatorAnswerScore();
+    Collections.sort(answerScoreList, comparator);
+    Collections.reverse(answerScoreList);
+    for (int i = 0; i < correctNuber && i < answerScoreList.size(); i++) {
+      result.add(answerScoreList.get(i));
+    }
+    return result;
+  }
+}
---
+++ hw2-xiangl2/src/main/java/edu/cmu/xiangl2/annotators/NGramAnswerScoring.java
@@ -0,0 +1,89 @@
+package edu.cmu.xiangl2.annotators;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIndex;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
+
+import edu.cmu.deiis.types.*;
+
+/**
+ * Analysis engine which assign a score to each answer to indicate how likely it could be a correct
+ * answer.
+ * <p>
+ * The scoring method is based on N-Gram hit rate between sentence and answer. A high score shows
+ * that the answer is likely to be a correct answer.
+ */
+public class NGramAnswerScoring extends JCasAnnotator_ImplBase {
+
+  /**
+   * The major scoring process.
+   * <p>
+   * This method assign a score to each answer to show how likely it could be a correct answer. The
+   * score is calculated using N-Gram hit rate. That is to say, the system counts the number of
+   * N-Gram annotations in the answer sentences that could be matched with an N-Gram annotations in
+   * the question sentence, then calculates the ratio of matched N-Gram annotation number to total
+   * N-Gram annotation number
+   */
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    // TODO Auto-generated method stub
+    FSIndex<?> questionIndex = aJCas.getAnnotationIndex(Question.type);
+    Iterator<?> questionIter = questionIndex.iterator();
+    List<NGram> questionNgram = null;
+    if (questionIter.hasNext()) {
+      questionNgram = getSentenceNgram(aJCas, (Annotation) questionIter.next());
+    }
+
+    FSIndex<?> answerIndex = aJCas.getAnnotationIndex(Answer.type);
+    Iterator<?> answerIter = answerIndex.iterator();
+    List<NGram> answerNgram = null;
+    while (answerIter.hasNext()) {
+      Answer answer = (Answer) answerIter.next();
+      answerNgram = getSentenceNgram(aJCas, (Annotation) answer);
+      int total = answerNgram.size();
+      int match = 0;
+      for (int i = 0; i < questionNgram.size(); i++) {
+        for (int j = 0; j < answerNgram.size(); j++) {
+          if (aJCas
+                  .getDocumentText()
+                  .substring(questionNgram.get(i).getBegin(), questionNgram.get(i).getEnd())
+                  .equals(aJCas.getDocumentText().substring(answerNgram.get(j).getBegin(),
+                          answerNgram.get(j).getEnd()))) {
+            match += 1;
+          }
+        }
+      }
+      AnswerScore annotation = new AnswerScore(aJCas);
+      annotation.setBegin(answer.getBegin());
+      annotation.setEnd(answer.getEnd());
+      annotation.setConfidence(1);
+      annotation.setCasProcessorId(this.getClass().getName());
+      annotation.setAnswer(answer);
+      annotation.setScore(match / (double) total);
+      annotation.addToIndexes();
+    }
+  }
+
+  private List<NGram> getSentenceNgram(JCas aJCas, Annotation annotation) {
+    List<NGram> result = new ArrayList<NGram>();
+    FSIndex<?> ngramIndex = aJCas.getAnnotationIndex(NGram.type);
+    Iterator<?> ngramIter = ngramIndex.iterator();
+    int begin, end;
+    begin = annotation.getBegin();
+    end = annotation.getEnd();
+    while (ngramIter.hasNext()) {
+      NGram ngram = (NGram) ngramIter.next();
+      if (ngram.getBegin() >= begin && ngram.getEnd() <= end) {
+        result.add(ngram);
+      }
+    }
+    return result;
+  }
+
+}
